fb399b546f3f1f79c890dc8eeeee65bbee020ec9
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2019-04-13 06:41:56 -0400

    cositas

--- JavaGit/src/controlador/C_Tecnico.java
+++ JavaGit/src/controlador/C_Tecnico.java
@@ -7,7 +7,6 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
 import modelo.M_Tecnico;
 import vista.V_Tecnico;
 
--- JavaGit/src/controlador/C_Tipo.java
+++ JavaGit/src/controlador/C_Tipo.java
@@ -7,7 +7,6 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
 import modelo.M_Tipo;
 import vista.V_Tipo;
 
